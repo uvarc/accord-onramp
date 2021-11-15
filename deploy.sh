4cba52663496dd334d1033d783e9ec7721e95f4c
Neal Magee <nem2p@virginia.edu>
2021-11-15 15:17:15 -0500

    Showing and hiding with red

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+set -e
+
 # sync the repo with S3
-aws --profile uvarc s3 sync . s3://accord-frontdoor/ --acl public-read --delete --exclude ".git/*" --exclude ".github/*"
+aws --profile uvarc s3 sync . s3://accord-frontdoor/ --acl public-read --delete --exclude ".git/*" --exclude ".github/*";
 
 # invalidate CF cache
-aws --profile uvarc cloudfront create-invalidation --distribution-id E33CY0JIMDZFTI --paths "/*"
+aws --profile uvarc cloudfront create-invalidation --distribution-id E33CY0JIMDZFTI --paths "/*";
 
+exit 0;
