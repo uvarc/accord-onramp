bbb7cb3e01f995de626b5f2c608d254bbc2bc125
Neal Magee <nem2p@virginia.edu>
2022-03-21 09:09:57 -0400

    Remove stray carat

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 # sync the repo with S3
-aws --profile uvarc s3 sync . s3://accord-frontdoor/ --acl public-read --delete --exclude ".git/*" --exclude ".github/*";
+aws --profile uvarc s3 sync . s3://accord-frontdoor/ --acl public-read --delete --exclude ".git/*" --exclude ".github/*" --exclude "api/*" --exclude "exp-imp/*";
 
 # invalidate CF cache
 aws --profile uvarc cloudfront create-invalidation --distribution-id E33CY0JIMDZFTI --paths "/*";
